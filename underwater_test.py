387650f709128395448fbc72c412bcd36b4f4652
Bosen Lin <linbosen083@gmail.com>
2026-03-09 15:02:06 +0800

    Update underwater_test.py

--- underwater_test.py
+++ underwater_test.py
@@ -51,11 +51,11 @@ def get_args():
     parser = argparse.ArgumentParser(description='Evaliation')
 
     ### >>> [data] set data info.
-    parser.add_argument('--database', default='TF_SUIM', type=str, help='database name (default: LIVE)')
+    parser.add_argument('--database', default='TIUIED', type=str, help='database name')
     parser.add_argument('--crop_size', type=int, default=256, help='input image size for training (default: 256)')
 
     ### >>> [model] set model info.
-    parser.add_argument('--model_E', default='my_UIE_mpvit_sa_mix', type=str, help='model name ((Wa)DIQaM-FR/NR, default: WaDIQaM-FR)')
+    parser.add_argument('--model_E', default='DTIUIE', type=str, help='model name')
     parser.add_argument('--pretrained', default=None, type=str, help='path to latest checkpoint (default: None)')
 
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
@@ -76,119 +76,36 @@ if __name__ == '__main__':
 
     # >>> [model] set model info.
     # >>> [model] set model info.
-    if args.model_E == 'my_UIE_mpvit_sa':
-        from models.enhancement.model_E_UNet_mpvit import TwoBranchNetwork_SA
+    if  args.model_E == 'TIUIED':
+        from models.enhancement.model_dtiuie import TIUIED
         model_E = TwoBranchNetwork_SA()
         model_E = model_E.to(device)
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_mpvit_sa_vgg/my_UIE_mpvit_sa-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-03-20-2025-16-24-05-best'
-    elif args.model_E == 'my_UIE_mpvit_sa_mix':
-        from models.enhancement.model_E_UNet_mpvit_mix_3mix import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_mpvit_sa_mix_vgg/my_UIE_mpvit_sa_mix-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-03-20-2025-16-24-19-best'
+        args.pretrained = './checkpoints/dtiuie_ckpt.pth'
     
-    
-    
-    elif args.model_E == 'my_UIE_mpvit_sa_mix_cross':
-        from models.enhancement.model_E_UNet_mpvit_cross import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)    
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_mpvit_sa_mix_vgg/my_UIE_mpvit_sa_mix-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-03-27-2025-12-09-26-best'
-    elif args.model_E == 'my_UIE_mpvit_sa_mix_single':
-        from models.enhancement.model_E_UNet_mpvit_mix_single import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)    
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_mpvit_sa_vgg/my_UIE_mpvit_sa-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-03-13-2025-10-17-32-best'
-    elif args.model_E == 'my_UIE_ab_sa_vgg13':
-        from models.enhancement.model_E_UNet_mpvit_mix import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_ab_sa_vgg13_vgg/my_UIE_ab_sa_vgg13-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-06-14-2025-14-31-39-best_ssim.pth'
-    elif args.model_E == 'my_UIE_ab_sa_res50':
-        from models.enhancement.model_E_UNet_mpvit_mix_res import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)    
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_ab_sa_res50_vgg/my_UIE_ab_sa_res50-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-06-14-2025-15-39-37-best_ssim.pth'
-
-    elif args.model_E == 'my_UIE_mpvit_sa_mix_suime':
-        from models.enhancement.model_E_UNet_mpvit_mix_3mix import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_mpvit_sa_mix_vgg/my_UIE_mpvit_sa_mix-my_UIE-SUIM_E-lr=0.0001-wd=0.001-bs=4-03-27-2025-00-33-21-best'
-
-    elif args.model_E == 'my_UIE_mpvit_sa_mix_no_mix_loss':
-        from models.enhancement.model_E_UNet_mpvit_mix import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_mpvit_sa_mix_vgg/my_UIE_mpvit_sa_mix-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-01-05-2026-12-39-58-best_ssim.pth'
-    elif args.model_E == 'my_UIE_mpvit_sa_mix_pixel_mix_loss':
-        from models.enhancement.model_E_UNet_mpvit_mix import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_mpvit_sa_mix_vgg/my_UIE_mpvit_sa_mix-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-01-05-2026-13-25-51-best_ssim.pth'
-
-    elif args.model_E == 'my_UIE_mpvit_sa_mix_all_mix_loss':
-        from models.enhancement.model_E_UNet_mpvit_mix import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_mpvit_sa_mix_vgg/my_UIE_mpvit_sa_mix-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-01-07-2026-10-43-28-best_ssim.pth'
-
-    elif args.model_E == 'my_UIE_mpvit_sa_mix_16_mix_loss':
-        from models.enhancement.model_E_UNet_mpvit_mix import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_mpvit_sa_mix_vgg/my_UIE_mpvit_sa_mix-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-01-08-2026-21-37-21-best_ssim.pth'
-    elif args.model_E == 'my_UIE_mpvit_sa_mix_no_train_loss':
-        from models.enhancement.model_E_UNet_mpvit_mix import TwoBranchNetwork_SA
-        model_E = TwoBranchNetwork_SA()
-        model_E = model_E.to(device)
-        args.pretrained = '/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/checkpoints/my_UIE_mpvit_sa_mix_vgg/my_UIE_mpvit_sa_mix-my_UIE-SUIM-lr=0.0001-wd=0.001-bs=4-01-12-2026-20-03-33-best_ssim.pth'
-
-
 
     from models.segmentation.vgg_unet import VGG16Unet, VGG13Unet
     from models.segmentation.resnet_unet import ResNet50Unet
 
     model_S = VGG16Unet(n_channels=3, n_classes=8, pretrained=True)
-    #model_S = ResNet50Unet(n_channels=3, n_classes=8, pretrained=True)
     model_S = model_S.to(device)
 
 
-
     from dataloader_seg import Tested_Set
-    # dataset_dir = '/home/catchacat/data/UnderwaterDatasets/UnderwaterDetection/SUIM/SUIM_fix/train_val/images/'
-    # dataset_dir = '/home/catchacat/data/UnderwaterDatasets/UnderwaterDetection/Seg_UIIS/UIIS/UDW/train/'
-    dataset_dir = '/home/catchacat/data/UnderwaterDatasets/UnderwaterEnhancement/UIEB/valid/images'
+    dataset_dir = './dataset/test/images'
     args.classes = 8
     dataset = Tested_Set(file_path = dataset_dir, status='valid', augmentation=False, angle=0, size_h=args.crop_size, size_w=args.crop_size, hflip_p=0)
     dataloader = torch.utils.data.DataLoader(dataset, shuffle=False, drop_last=True, batch_size=1, num_workers=8, pin_memory=True)
 
-    # logging.info(f'model params: %d' % count_parameters(model))
-    # print('there are total %s batches for train' % (len(train_loader)))
-    # print('there are total %s batches for val' % (len(val_loader)))
-    # # >>> [pretrained] reload pretrained models
     if args.pretrained:
         pre_trained_state_dict = torch.load(args.pretrained)
-        # for key in pre_trained_state_dict['model_E'].keys():
-        #     print(key)
-     
         model_E.load_state_dict(pre_trained_state_dict['model_E'])
         model_S.load_state_dict(pre_trained_state_dict['model_S'])
-        #start_epoch = ckpt['epoch'] + 1
-
     model_E.to(device=device), model_S.to(device=device)
-
-    # >>> print model
-    # image = torch.randn(1,3,384,384).cuda()
-    # writer.add_graph(model, (image,))
 
     model_E.eval(), model_S.eval()
     num_val_batches = len(dataloader)
 
     epoch_valid_psnr = []
-        
-    # starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
-    # times = []
 
     with torch.no_grad():
         for batch in tqdm(dataloader, total=num_val_batches, desc='Validation round', unit='batch', ncols=150, leave=True):
@@ -209,19 +126,10 @@ if __name__ == '__main__':
             # Generate output
             if 'sa' in args.model_E:
                 _, feat_raw = model_S(images, return_feats=True)
-                # starter.record()
                 outputs = model_E(images, feat_raw)
-                # ender.record()
             else:
                 outputs = model_E(images)
-            # temp_uciqe, N = compute_uciqe(outputs)
-            # valid_average_uciqe.update(temp_uciqe, N)
-            # temp_uiqm, N = compute_uiqm(outputs)
-            # valid_average_uiqm.update(temp_uiqm, N)
 
-            # torch.cuda.synchronize()
-            # elapsed_time_ms = starter.elapsed_time(ender)
-            # times.append(elapsed_time_ms)
 
             if args.draw_images:
                 for i, (output, image_name) in enumerate(zip(outputs, images_names)):
@@ -233,16 +141,8 @@ if __name__ == '__main__':
                     output = (output * 255).astype(np.uint8)
                     output = Image.fromarray(output)
                     
-                    # image.save(f'{args.save_dir}/{args.model}/{os.path.basename(image_name)}_src.png')
-                    # output.save(f'/home/catchacat/data/UnderwaterDatasets/UnderwaterDetection/SUIM/Enhance/my_TFUIE_notrain_loss/train_val/{os.path.basename(image_name)}')
-                    # output.save(f'/home/catchacat/data/UnderwaterDatasets/UnderwaterDetection/Seg_UIIS/Enhance/Enhance_FullSize/train/{os.path.basename(image_name)}')
-                    output.save(f'/home/catchacat/workspace/Underwater_enhancement/my_TFUIE_new/outputs/UIEB_my_UIE_mpvit_sa_mix/{os.path.basename(image_name)}')
+                    output.save(f'./outputs/DTIUIE/{os.path.basename(image_name)}')
 
 
-                    
-                    # output.save(f'{args.save_dir}/{args.model}/{os.path.basename(image_name)}_output.png')
-                    # image_gt.save(f'{args.save_dir}/{args.model}/{os.path.basename(image_name)}_gt.png')
-
-    # print(f"Average inference time: {np.mean(times):.3f} ms")
     
 
